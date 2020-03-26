dcccd71cb4d082df157762e04eb7857623b4d184
M@T <matteo.rosadini.91@gmail.com>
2020-03-26 14:50:53 +0200

    added html and bootstr

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(express.static('public')); // to overcome CORS
+app.use(bodyParser.urlencoded({extended: true})); // to make the app able to read requests with json 
+app.use(bodyParser.json()); // to make the app able to read requests with json 
 
 app.get('/', (req, res) => {
 	res.send('hello');
