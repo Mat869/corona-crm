76b918c56aa3f73abaf538e822a5bdd206a0b5bc
M@T <matteo.rosadini.91@gmail.com>
2020-03-27 17:17:28 +0300

    restAPI + validation

--- src/server.js
+++ src/server.js
@@ -1,14 +1,75 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const validation = require('./validation');
 const port = 3000;
 
 app.use(express.static('public')); // to overcome CORS
 app.use(bodyParser.urlencoded({extended: true})); // to make the app able to read requests with json 
 app.use(bodyParser.json()); // to make the app able to read requests with json 
 
+
+let customers = [];
+
 app.get('/', (req, res) => {
 	res.send('hello');
+});
+
+app.put('/customer', (req, res) => {
+    //console.log(req.body);
+    if(!validation(req.body.name, req.body.email, req.body.birthdate, req.body.creationDate, req.body.notes, req.body.overEighteen)) {
+        res.status(400).send();
+        return;
+    }
+    customers.push({
+        id: users.length + 1,
+        name: req.body.name,
+		email: req.body.email,
+		birthdate: req.body.birthdate,
+		creationDate: null,
+		notes: req.body.notes,
+		overEighteen: false
+    });
+    res.status(201).send();
+});
+
+app.get('/customer/', (req, res) => {
+    res.json(customers);
+});
+
+app.get('/customer/:id', (req, res) => {
+    const requestedCustomer = customers.find(customer => { 
+        return customer.id === parseInt(req.params.id); 
+    });
+    if (!requestedCustomer) { 
+        res.status(404).send();
+        return;
+    }
+    res.status(200).json(requestedCustomer);
+});
+
+app.post('/customer/:id', (req, res) => {
+    const requestedCustomer = customers.find(customer => {
+        return customer.id === parseInt(req.params.id);
+    });
+    if(!requestedCustomer) {
+        res.status(404).send();
+        return;
+    }
+    res.status(200).send();
+});
+
+app.delete('/customer/:id', (req, res) => {
+    const requestedCustomer = customers.find(customer => {
+        return customer.id === parseInt(req.params.id);
+    });
+    if (!requestedCustomer) {
+        res.status(404).send();
+        return;
+    }
+    const index = customers.indexOf(requestedCustomer);
+    customers.splice(index, 1);
+    res.status(204).send();
 });
 
 app.listen(port, () => {
---
+++ src/validation.js
@@ -0,0 +1,98 @@
+class Form {
+
+    isName (str) {
+        if(str.indexOf(' ') !== -1) {
+            return true;
+        }
+        return false;
+    }
+
+    minLength(str, min) {
+        if(str.length >= min) {
+            return true;
+        }
+        return false;
+    }
+
+    maxLength(str, max) {
+        if(str.length <= max) {
+            return true;
+        }
+        return false;
+    }
+
+    isEmail(str) {
+        if(str.indexOf(' ') === -1 && str.indexOf('_') === -1 && str.indexOf('@') !== -1) {
+            return true;
+        }
+        return false;
+    }
+
+    isPassword(str) {
+        let numbers = /[0-9]/g;
+        let letters = /[a-z]/g;
+        if(str.match(numbers) && str.match(letters)) {
+            return true;
+        }
+        return false;
+    }
+
+    notes(str) {
+        let letters = /[a-z]/g;
+        if(str.match(letters)) {
+            return true;
+        }
+        return false;
+    }
+
+    isOverEighteen(value) {
+        if(value === true) {
+            return true;
+        }
+        return false;
+    }
+
+    isDate(date) {
+        let newDate = date.split('/');
+        let day = parseInt(newDate[0]);
+        let month = parseInt(newDate[1]);
+        let year = parseInt(newDate[2]);
+        if(month === 2 && day >29) {
+            return false;
+        }
+        return day >=1 &&
+          day <=31 &&
+          month >=1 &&
+          month <= 12 &&
+          year >= 1920 &&
+          year <= 2030;
+    }
+}
+
+class RegForm extends Form {
+    constructor(name, email, birthdate, creationDate, notes, overEighteen) {
+        super();
+        this.name = name;
+        this.email = email;
+        this.birthdate = birthdate;
+        this.creationDate = creationDate;
+        this.notes = notes;
+        this.overEighteen = overEighteen;
+    }
+    validate() {
+        return this.isName(this.name) && 
+        this.minLength(this.password, 2) && 
+        this.maxLength(this.password, 16) && 
+        this.isEmail(this.email) && 
+        this.isDate(this.birthdate) && 
+        this.isOverEighteen(this.overEighteen)
+    }
+}
+
+function validation(name, email, birthdate, creationDate, notes, overEighteen) {
+    let newCustomer = new RegForm(name, email, birthdate, creationDate, notes, overEighteen);
+    return newCustomer.validate();
+}
+
+
+module.exports = validation;
